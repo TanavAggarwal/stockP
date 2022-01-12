629a208ac39e79863dfa055ef01f8bfdfdae14ef
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2022-01-12 15:52:57 +0530

    Update settings.py

--- stockP/settings.py
+++ stockP/settings.py
@@ -161,6 +161,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 django_heroku.settings(locals())
