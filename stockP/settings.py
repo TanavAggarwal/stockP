5b51a40a6311f91e5b2556f0df0313be5e15a2f7
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2022-01-12 15:39:00 +0530

    Update settings.py

--- stockP/settings.py
+++ stockP/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,3 +162,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
