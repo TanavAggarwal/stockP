7a708a04f39129284fe90d570104f7d8865b40f8
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2022-01-12 15:13:03 +0530

    Update settings.py

--- stockP/settings.py
+++ stockP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,6 +116,7 @@ DATABASES = {
         }
     }
 }
+DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
 # <password>
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
