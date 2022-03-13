3ae03e74d11beb34e94eae45528f9c997a88cb10
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2022-03-13 19:31:37 +0530

    Update urls.py

--- stockapp/urls.py
+++ stockapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('settings.html/', views.settings, name='settings'),
     path('index.html/', views.index, name='index'),
     path('save/', views.save_data, name='save'),
     path('save2/', views.save_data2, name='save2'),
@@ -11,11 +12,12 @@ urlpatterns = [
     path('logout.html/', views.logout, name='logout'),
     path('register.html/', views.register, name='register'),
     path('password.html/', views.password, name='password'),
-    path('charts.html/', views.charts, name='charts'),
     path('predictor.html/', views.predictor, name='predictor'),
     path('refresh_charts.html/', views.refresh_charts, name='refresh_charts'),
     path('refresh_funds.html/', views.refresh_funds, name='refresh_funds'),
     #path('mfh_charts.html/', views.mfh_charts, name='mfh_charts'),
     path('mfunds.html/', views.mfunds, name='mfunds'),
     path('mfholding.html/', views.mfholding, name='mfholding'),
+    path('DelEq.html/', views.deleq, name='DelEq'),
+    path('DelMf.html/', views.delmf, name='DelMf'),
 ]
