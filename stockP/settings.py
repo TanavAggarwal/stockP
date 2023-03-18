e66de599518ba43df824319767e461dc551ebe01
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2023-03-19 00:54:04 +0530

    Update settings.py

--- stockP/settings.py
+++ stockP/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 #import dj_database_url
-import django_on_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
